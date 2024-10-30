0111d3844ef7fd6b0797266066718b857c357e3e
AnnSofie <annsofie.sjolander@gmail.com>
2024-10-30 14:35:22 +0100

    ResponseResults and ResponseFactories created

---
+++ API-Users-RIKA-WIN23.Infrastructure/Utilities/ResponseResult.cs
@@ -0,0 +1,19 @@
+﻿
+namespace API_Users_RIKA_WIN23.Infrastructure.Utilities;
+
+public enum StatusCode
+{
+    OK = 200,
+    CREATED = 201,
+    ERROR = 400,
+    UNAUTHORIZED = 401,
+    NOT_FOUND = 404,
+    EXISTS = 409    
+}
+
+public class ResponseResult
+{
+    public StatusCode StatusCode { get; set;}
+    public object? ContentResult { get; set;}
+    public string? Message { get; set;}
+}
