e0561fce7f79db07c584a51699fd6ae4a6957874
Bjorn-23 <bjornandersson84@gmail.com>
2024-10-31 13:59:58 +0100

    Added various factories

---
+++ API-Users-RIKA-WIN23.Infrastructure/Factories/ShoppingCartFactory.cs
@@ -0,0 +1,49 @@
+﻿using API_Users_RIKA_WIN23.Infrastructure.DTOs;
+using API_Users_RIKA_WIN23.Infrastructure.Entities;
+
+namespace API_Users_RIKA_WIN23.Infrastructure.Factories;
+
+public class ShoppingCartFactory
+{
+    public static UserShoppingCartDto Create(UserShoppingCartEntity entity)
+    {
+        return new UserShoppingCartDto
+        {
+            Id = entity.Id,
+            UserId = entity.UserId,
+            Products = ShoppingCartItemFactory.Create(entity.Products)
+        };
+    }
+
+    public static UserShoppingCartEntity Create(UserShoppingCartDto dto)
+    {
+        return new UserShoppingCartEntity
+        {
+            Id = dto.Id,
+            UserId = dto.UserId,
+            Products = ShoppingCartItemFactory.Create(dto.Products)
+        };
+    }
+
+    public static ICollection<UserShoppingCartDto> Create(ICollection<UserShoppingCartEntity> entities)
+    {
+        var dtos = new List<UserShoppingCartDto>();
+        foreach (var entity in entities)
+        {
+            dtos.Add(Create(entity));
+        }
+
+        return dtos;
+    }
+
+    public static ICollection<UserShoppingCartEntity> Create(ICollection<UserShoppingCartDto> dtos)
+    {
+        var entities = new List<UserShoppingCartEntity>();
+        foreach (var dto in dtos)
+        {
+            entities.Add(Create(dto));
+        }
+
+        return entities;
+    }
+}
---
+++ API-Users-RIKA-WIN23.Infrastructure/Factories/UserFactory.cs
@@ -0,0 +1,31 @@
+﻿using API_Users_RIKA_WIN23.Infrastructure.DTOs;
+using API_Users_RIKA_WIN23.Infrastructure.Entities;
+
+namespace API_Users_RIKA_WIN23.Infrastructure.Factories;
+
+public class UserFactory
+{
+    public static UserEntity Create(UserDto dto)
+    {
+        return new UserEntity
+        {
+            IsExternalAccount = dto.IsExternalAccount,
+            Profile = UserProfileFactory.Create(dto.Profile!) ?? null,
+            Address = AddressFactory.Create(dto.Address!) ?? null,
+            WishList = WishListFactory.Create(dto.WishList!) ?? null,
+            ShoppingCarts = ShoppingCartFactory.Create(dto.ShoppingCarts!) ?? null,
+        };
+    }
+
+    public static UserDto Create(UserEntity entity)
+    {
+        return new UserDto
+        {
+            IsExternalAccount = entity.IsExternalAccount,
+            Profile = UserProfileFactory.Create(entity.Profile!) ?? null,
+            Address = AddressFactory.Create(entity.Address!) ?? null,
+            WishList = WishListFactory.Create(entity.WishList!) ?? null,
+            ShoppingCarts = ShoppingCartFactory.Create(entity.ShoppingCarts!) ?? null,
+        };
+    }
+}
